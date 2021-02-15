622d08a675f869d7552108cb495414acf141db04
anata-san <anatasan0828@gmail.com>
2021-02-16 01:33:42 +0900

    Add Table

---
+++ db/migrate/20210215161925_create_follows.rb
@@ -0,0 +1,12 @@
+class CreateFollows < ActiveRecord::Migration[6.0]
+  def change
+    create_table :follows do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :follow, null: false, foreign_key: { to_table: :users }
+
+      t.timestamps
+    end
+
+    add_index :follows, [:user_id, :follow_id], unique: true
+  end
+end
